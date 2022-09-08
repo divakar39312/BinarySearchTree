e77acf28e267ed254771a17a3aed8e9e7a016819
Divakar <divakar39312@gmail.com>
2022-09-08 23:13:40 +0530

    [Divakar]:search node

--- src/com/Bridgelabz/BinarySearchTree.java
+++ src/com/Bridgelabz/BinarySearchTree.java
@@ -7,9 +7,20 @@ public class BinarySearchTree {
 	 * 
 	 */
 	Node root;
+	boolean flag = true;
 	
 	public void addNode(int data) {
-				Node node = new Node(data);
+		/**
+		 * Node Object Created with Constructor Passing data
+		 * If Else Condition Needed.
+		 * If - root equal to null, root = node and returns from function
+		 * Else - parent and current stores root
+		 * 		  While Looping till current node equals null.
+		 * While Loop - If   - node is greater than present data it moves to right leaf node
+		 * 				Else - data moves to left leaf node.
+		 * 
+		 */
+		Node node = new Node(data);
 		node.data = data;
 		node.left = null;
 		node.right = null;
@@ -38,6 +49,17 @@ public class BinarySearchTree {
 			}
 		}
 	}
+	//To Print Tree its Function
+	public void printTree(Node node) {
+		if(node.left != null) {
+			printTree(node.left);
+		}
+		System.out.print(node.data+" ");
+		if(node.right != null) {
+			printTree(node.right);
+		}
+	}
+	
 	
 	public class Node {
 		/*
@@ -54,15 +76,25 @@ public class BinarySearchTree {
 		
 
 	}
-	//To Print Tree its Function
-	public void printTree(Node node) {
-		if(node.left != null) {
-			printTree(node.left);
+	//To Search Node Function
+	public void searchNode(Node node, int value) {
+		if(root == null) {
+			System.out.println("Tree is Empty");	
+			return;
 		}
-		System.out.println(node.data+" ");
-		if(node.right != null) {
-			printTree(node.right);
+		else {
+			if(node.data == value) {
+				flag = true;
+				return;
+			}
+			if(flag == false && node.left != null) {
+				searchNode(node.left, value);
+			}
+			if(flag == false && node.right != null) {
+				searchNode(node.right, value);
+			}
 		}
+		
 	}
 
 }
--- src/com/Bridgelabz/TreeMain.java
+++ src/com/Bridgelabz/TreeMain.java
@@ -3,28 +3,47 @@ package com.Bridgelabz;
 import java.util.Scanner;
 
 public class TreeMain {
-	
+	static boolean flag = true;
 	public static void main(String[] args) {
-			welcome();
+	/*
+	 * 
+	 * For UC2- Elements Given in Figure are 
+	 * 56,30,70,22,40,60,95,65,11,3,16,63,67
+	 * 
+	 */
+		welcome();
+		Scanner sc = new Scanner(System.in);
 		BinarySearchTree tree = new BinarySearchTree();
-		tree.addNode(56);
-		tree.addNode(30);
-		tree.addNode(70);
-		System.out.println("Binary Search Tree After Adding Elements ");
-		tree.printTree(tree.root);
-		tree.addNode(22);
-		tree.addNode(40);
-		tree.addNode(11);
-		tree.addNode(3);
-		tree.addNode(16);
-		tree.addNode(60);
-		tree.addNode(95);
-		tree.addNode(65);
-		tree.addNode(63);
-		tree.addNode(67);
-		System.out.println("After Adding Remaining Elements into Tree");
-		tree.printTree(tree.root);
-
+		while(true) {
+			System.out.println("\nSelect \n1.Add Element\n2.PrintTree \n3.Search Element \n4.Exit");
+			int option = sc.nextInt();
+			switch(option) {
+			case 1:
+				System.out.println("Please Enter Element:");
+				tree.addNode(sc.nextInt());
+				break;
+			case 2:
+				
+				System.out.println("After Adding Remaining Elements into Tree");
+				tree.printTree(tree.root);
+				break;
+			case 3 :
+				System.out.println("Please Enter Element To Search :");
+				int val = sc.nextInt();
+				tree.searchNode(tree.root,val);
+				if(flag) {
+					System.out.println(val+" Element is Avilable");
+				}
+				else {
+					System.out.println(val+" Element Not Available");
+				}
+				break;
+			case 4:
+				System.exit(0);
+			default:
+				System.out.println("Invalid Option");	
+			}
+		}
 	}
 	
 	//Welcome Function
